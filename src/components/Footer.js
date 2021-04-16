58a835e9146d03f65ad4868cb348098c75cd308e
Anna <anlautens@gmail.com>
2021-04-16 15:08:39 -0400

    Changing footer link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Built by Anna McIntosh | '}
-      <Link color="inherit" href="https://github.com/annamcintosh/portfolio">
+      {"Built by Anna McIntosh | "}
+      <Link
+        color="inherit"
+        target="_blank"
+        href="https://github.com/annamcintosh/portfolio"
+      >
         Code for Nerds
       </Link>
     </Typography>
@@ -29,8 +33,14 @@ export default function Footer(props) {
   return (
     <footer className={classes.footer}>
       <Container maxWidth="lg">
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          Thanks for visiting! I'd love to talk more with you about how we might work together.
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          Thanks for visiting! I'd love to talk more with you about how we might
+          work together.
         </Typography>
         <Copyright />
       </Container>
